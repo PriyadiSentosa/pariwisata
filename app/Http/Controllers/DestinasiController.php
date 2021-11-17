e3e79061674bb268f93b59a94b08a1fe244e3ff6
PriyadiSentosa <priyadisentosa825@gmail.com>
2021-11-17 08:09:48 +0700

    Project Wisata

---
+++ app/Http/Controllers/DestinasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\destinasi;
+use Illuminate\Http\Request;
+
+class DestinasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\destinasi  $destinasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(destinasi $destinasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\destinasi  $destinasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(destinasi $destinasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\destinasi  $destinasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, destinasi $destinasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\destinasi  $destinasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(destinasi $destinasi)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_11_16_134502_create_destinasis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDestinasisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinasis', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('alamat_id')->unsigned();
+            $table->text('alamat');
+
+            $table->foreign('alamat_id')->references('id')
+                ->on('wisatas')->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destinasis');
+    }
+}
